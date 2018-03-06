a97f2e8ac2a5d712499cbc1af25a16a65a15dc47
Tom Vorel <tomvorel13@gmail.com>
2018-03-06 20:41:06 +0100

    Final project

---
+++ src/data/stocks.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "BMW",
+    price: 110
+  },
+  {
+    id: 2,
+    name: "Google",
+    price: 100
+  },
+  {
+    id: 3,
+    name: "Twitter",
+    price: 150
+  },
+  {
+    id: 4,
+    name: "Tesla",
+    price: 260
+  }
+];
